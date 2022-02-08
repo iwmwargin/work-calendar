68c05a4e7a78f91b6574975e38f695736a2788b6
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-02-07 21:53:06 -0500

    added some commenting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 // This code prepares the JavaScript to be ready once the Dom is ready
 $(document).ready(function () {
     // display current day and time
@@ -10,7 +9,6 @@ $(document).ready(function () {
         //set items to local storage.
         localStorage.setItem(hour, todo);
     });
-
     // pulling all items from localStorage
     $("#9 .description").val(localStorage.getItem("9"));
     $("#10 .description").val(localStorage.getItem("10"));
@@ -21,12 +19,12 @@ $(document).ready(function () {
     $("#3 .description").val(localStorage.getItem("3"));
     $("#4 .description").val(localStorage.getItem("4"));
     $("#5 .description").val(localStorage.getItem("5"));
-
-   
 })
+
+// function that will run else if statements to figure out time and apply the color accordingly
 $(".description").each(function () {
     var hours = parseInt(moment().format("H"));
-if(hours > 9 && hours <= 17){
+if(hours = 0 && hours <= 1){
     $(".description").addClass("present");
 }
 else if(hours < 9){
