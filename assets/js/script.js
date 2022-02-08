3455d91d041bd423feb2d32280803f42bb62707d
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-02-08 09:02:30 -0500

    finally got the time to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,45 @@
 // This code prepares the JavaScript to be ready once the Dom is ready
 $(document).ready(function () {
-    // display current day and time
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-      
-      $(".saveBtn").click(function () {
-        var todo = $(this).siblings(".description").val(); // Grab the text input into textarea
-        var hour = $(this).parent().attr("id"); // Grab the hour
-        //set items to local storage.
-        localStorage.setItem(hour, todo);
-    });
-    // pulling all items from localStorage
-    $("#9 .description").val(localStorage.getItem("9"));
-    $("#10 .description").val(localStorage.getItem("10"));
-    $("#11 .description").val(localStorage.getItem("11"));
-    $("#12 .description").val(localStorage.getItem("12"));
-    $("#1 .description").val(localStorage.getItem("1"));
-    $("#2 .description").val(localStorage.getItem("2"));
-    $("#3 .description").val(localStorage.getItem("3"));
-    $("#4 .description").val(localStorage.getItem("4"));
-    $("#5 .description").val(localStorage.getItem("5"));
-})
-var time = moment();
+  // display current day and time
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+  $(".saveBtn").click(function () {
+    var todo = $(this).siblings(".description").val(); // Grab the text input into textarea
+    var hour = $(this).parent().attr("id"); // Grab the hour
+    //set items to local storage.
+    localStorage.setItem(hour, todo);
+  });
+  // pulling all items from localStorage
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("1"));
+  $("#14 .description").val(localStorage.getItem("2"));
+  $("#15 .description").val(localStorage.getItem("3"));
+  $("#16 .description").val(localStorage.getItem("4"));
+  $("#17 .description").val(localStorage.getItem("5"));
+});
+
+var currentTime = new Date();
+let hours = currentTime.getHours();
+
 // function that will run else if statements to figure out time and apply the color accordingly
-$(".description").each(function () {
-    var hours = parseInt(moment().format("H"));
-if(hours > 9 && hours <= 17){
-    $(".description").addClass("present");
-    $(".description").removeClass("past");
-    $(".description").removeClass("future");
-}
-else if(hours < 9){
-    $(".description").addClass("past");
-    $(".description").removeClass("present");
-    $(".description").removeClass("future");
-}
-else {
-    $(".description").addClass("future");
-    $(".description").removeClass("present");
-    $(".description").removeClass("past");
-}
-
-})
-
-
+$(".time-block").each(function () {
+  var time = parseInt($(this).attr("id"))
+  
+  if (time === hours) {
+    $(this).addClass("present");
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+  } else if (time < hours) {
+    $(this).addClass("past");
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+  } else {
+    $(this).addClass("future");
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+  }
+});
 
